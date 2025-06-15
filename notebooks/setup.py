14b5b061c968c4e14f40e5262601f6a0306cfa13
aaronbernal28 <aaronbernal28@gmail.com>
2025-06-15 17:33:44 -0300

    update

--- notebooks/setup.py
+++ notebooks/setup.py
@@ -1,12 +1,30 @@
 import torch
+from sklearn.model_selection import train_test_split
 
 # Configuración del dispositivo
 device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
 print(f"Usando dispositivo: {device}")
 torch.cuda.manual_seed(28)
-torch.cuda.set_per_process_memory_fraction(0.5)
+torch.cuda.set_per_process_memory_fraction(0.6)
 
 # Funciones auxiliares
+def train_test_split_path():
+    '''
+    Division fija de los datos de entrenamiento y testeo para todos los experimentos
+    '''
+    X_path = []
+    y = []
+    genres = ['blues', 'classical', 'country', 'disco', 'hiphop', 'jazz', 'metal', 'pop', 'reggae', 'rock']
+    for id, genre in enumerate(genres):
+        for i in range(100):
+            if not (i == 54 and genre == 'jazz'):
+                path = f'../Data/genres_original/{genre}/{genre}.000{i:02}.wav'
+                X_path.append(path)
+                y.append(id)
+    X_train_path, X_eval_path, y_train, y_eval = train_test_split(X_path, y, test_size=0.1, random_state=28, stratify=y)
+    X_train_path, X_test_path, y_train, y_test = train_test_split(X_train_path, y_train, test_size=0.2, random_state=28, stratify=y_train)
+    return X_train_path, X_test_path, X_eval_path, y_train, y_test, y_eval
+
 def train(model, train_dataloader, val_dataloader, optimizer, criterion, NUM_EPOCHS):
     train_losses = []
     val_losses = []
